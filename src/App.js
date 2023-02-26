619ea7931ecf5220fa653ac02c1ad53ffb5e690c
manjirimardikar <manjari.mardikar28@gmail.com>
2023-02-26 21:21:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Brands } from './Components/Brands/Brands';
+import { Credexperience } from './Components/Credexperience/Credexperience';
+import { Credsecurity } from './Components/Credsecurity/Credsecurity';
+import { Credstory } from './Components/Credstory/Credstory';
+import { Feelspecial } from './Components/Feelspecial/Feelspecial';
+import { Header } from './Components/Header/Header';
+import { Headersection } from './Components/Headersection/Headersection';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Headersection />
+      
+      <Feelspecial/>
+      <Brands />
+      <Credexperience />
+      
+      <Credsecurity />
+      <Credstory />
+     
+     
     </div>
   );
 }
---
+++ src/Components/Headersection/Headersection.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './Headersection.css'
+
+
+export const Headersection = () => {
+  return (
+    <div className='headsection'>
+        <div className="heading">
+        <h1>rewards for paying
+credit card bills.</h1>
+        </div>
+
+        <h2>join 9M+ members who win rewards and cashbacks everyday</h2>
+        
+       
+    </div>
+  )
+}
